5131e45255485213e688f2ffb7c0e5cc9d1fbca2
Tushar Pokle <tushar.pokle@gmail.com>
2017-04-19 01:47:17 +1000

    init

---
+++ parser-test.js
@@ -0,0 +1,78 @@
+const {expect} = require('chai');
+const {literal, optional, and, param} = require('./parser');
+
+describe('parse literal', () => {
+  const blahParser = literal('blah');
+
+  it('should parse blah', () => {
+    expect(blahParser({input: 'blah', ast: {}})).to.deep.equal({
+      input: '',
+      ast: {},
+    });
+  });
+
+  it('should parse blahfoo, leaving foo and ast', () => {
+    expect(blahParser({input: 'blahfoo', ast: {a: 1}})).to.deep.equal({
+      input: 'foo',
+      ast: {a: 1},
+    });
+  });
+
+  it('should not parse gah', () => {
+    expect(blahParser({input: 'gah', ast: {}})).to.deep.equal({
+      error: 'expected blah, but got gah...',
+    });
+  });
+});
+
+describe('parse optional', () => {
+  const parser = optional(literal('blah'));
+
+  it('should parse optional literal blah', () => {
+    expect(parser({input: 'blahfoo', ast: {a: 1}})).to.deep.equal({
+      input: 'foo',
+      ast: {a: 1},
+    });
+  });
+
+  it('should parse gah by ignoring it', () => {
+    expect(parser({input: 'gah', ast: {}})).to.deep.equal({
+      input: 'gah',
+      ast: {},
+    });
+  });
+});
+
+describe('parse and', () => {
+  const parser = and(literal('a'), literal('b'), literal('c'));
+
+  it('should parse abcd, leaving behind d', () => {
+    expect(parser({input: 'abcd', ast: {}})).to.deep.equal({
+      input: 'd',
+      ast: {},
+    });
+  });
+
+  it('should not parse abdef', () => {
+    expect(parser({input: 'abdef', ast: {}})).to.deep.equal({
+      error: 'expected c, but got d...',
+    });
+  });
+});
+
+describe('parse param', () => {
+  const parser = param('userid', /^\d{1,3}/, parseInt);
+
+  it('should parse 12f, taking 12, and leaving behind f', () => {
+    expect(parser({input: '12f', ast: {a: 1}})).to.deep.equal({
+      input: 'f',
+      ast: {userid: 12, a: 1},
+    });
+  });
+
+  it('should not parse abdef', () => {
+    expect(parser({input: 'abdef', ast: {a: 1}})).to.deep.equal({
+      error: 'Did not match /^\\d{1,3}/',
+    });
+  });
+});
---
+++ parser.js
@@ -0,0 +1,78 @@
+
+function literal(lit) {
+  return function({input, ast}) {
+    if (input.indexOf(lit) === 0) {
+      return {input: input.substr(lit.length), ast};
+    } else {
+      return {
+        error: `expected ${lit}, but got ${input.substr(0, lit.length)}...`,
+      };
+    }
+  };
+}
+
+function optional(child) {
+  return function(arg) {
+    const result = child(arg);
+    if (result.error) {
+      return arg; // as if nothing happened
+    } else {
+      return result;
+    }
+  };
+}
+
+function and() {
+  const children = arguments;
+  return function(arg) {
+    let nextArg = arg;
+    for(let i=0; i < children.length; i++) {
+      const child = children[i];
+      nextArg = child(nextArg);
+      if (nextArg.error) {
+        return nextArg;
+      }
+    }
+    return nextArg;
+  };
+}
+
+function assoc(obj, key, val) {
+  const kv = {};
+  kv[key] = val;
+  return Object.assign({}, obj, kv);
+}
+
+function param(name, re = /^[a-z]+/, parse = String) {
+  return function({input, ast}) {
+    const match = re.exec(input);
+    if (match) {
+      return {
+        input: match.input.substr(match.index + match[0].length),
+        ast: assoc(ast, name, parse(match[0])),
+      };
+    } else {
+      return {error: 'Did not match ' + re};
+    }
+  };
+}
+
+function exhaustive(child) {
+  return function(arg) {
+    const result = child(arg);
+    if (result.input.length > 0) {
+      return { error: 'Was meant to be exhaustive, but look at this remainder: ' + result.input };
+    } else {
+      return result;
+    }
+  }
+
+}
+
+module.exports = {
+  literal,
+  optional,
+  and,
+  param,
+  exhaustive
+};
